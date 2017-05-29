b33d188c3efc106b3dc50ceb2af7a9a419fc080d
Matangi <mtkendapady@gmail.com>
2017-05-29 00:09:01 -0500

    Basics of react built

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,44 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      homeLink: "Home"
+    };
+  }
+
+  onGreet() {
+    alert("Hello!");
+  }
+
+  onChangeLinkName(newName) {
+    this.setState({
+      homeLink: newName
+    });
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <h1>Hello!</h1>
+            <Header homeLink={this.state.homeLink}/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Home
+              name={"Matangi"}
+              initialAge={27}
+              greet={this.onGreet}
+              changeLink={this.onChangeLinkName.bind(this)}
+              initialLinkName={this.state.homeLink}
+            />
           </div>
         </div>
       </div>
